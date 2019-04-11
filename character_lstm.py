d069e21ae5d81636ac1428785c798abcfb870360
Michelle Evans Bouchet <michelle.evans-bouchet@student.ecp.fr>
2019-04-11 23:10:25 +0200

    Add embedding

--- character_lstm.py
+++ character_lstm.py
@@ -11,35 +11,44 @@ from parse_fics import Fanfic
 # Hyper params
 seq_length = 200
 batch_size = 100
-input_size = 1
+input_size = 30
 hidden_size = 100
 num_layers = 2
-num_epochs = 20
+num_epochs = 1
 learning_rate = 0.01
 dropout = 0
 
+
 class LSTMNet(torch.nn.Module):
 
-    def __init__(self,in_size,hidden_size, nb_layer, nb_classes, device, dropout):
-        super(LSTMNet,self).__init__()
+    def __init__(self, in_size, hidden_size, nb_layer, nb_classes, device, dropout):
+        super(LSTMNet, self).__init__()
         self.hidden_size = hidden_size
         self.nb_layer = nb_layer
         self.nb_classes = nb_classes
-        self.lstm = torch.nn.LSTM(in_size, hidden_size, nb_layer, batch_first=True, dropout = dropout)
+        self.emb = torch.nn.Embedding(nb_classes, in_size)
+        self.lstm = torch.nn.LSTM(
+            in_size, hidden_size, nb_layer, batch_first=True, dropout=dropout)
         self.fc = torch.nn.Linear(hidden_size, nb_classes)
         self.device = device
 
-    def forward(self,x, batch_size=batch_size):
+    def forward(self, x, batch_size=batch_size):
+        # print(f'x: {x.shape}')
+        # -1 corresponds to batch size (but is smaller for last batch)
+        x = self.emb(x).view(-1, seq_length, input_size)
         # initial states
-        h0 = torch.zeros(self.nb_layer, x.size(0), self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.nb_layer, x.size(0), self.hidden_size).to(self.device)
+        # print(f'x: {x.shape}')
+        h0 = torch.zeros(self.nb_layer, x.size(
+            0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.nb_layer, x.size(
+            0), self.hidden_size).to(self.device)
 
-        out,_ = self.lstm(x, (h0,c0))
-        out = self.fc(out[:,-1,:])
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
         return out
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     def to_one_hot(i, total_classes):
         return np.eye(total_classes)[i]
 
@@ -48,11 +57,11 @@ if __name__ == "__main__":
     char_to_int = {}
 
     # Load Fanfics, 49999 in total
-    with open("./fics.pkl", 'rb') as file: 
+    with open("./fics.pkl", 'rb') as file:
         fics = pickle.load(file)
-        fics = fics[:1] # begin with only this much
+        fics = fics[:1]  # begin with only this much
 
-    # Prepare Vocabulary 
+    # Prepare Vocabulary
     for fic in fics:
         chars.update(set(fic.body))
 
@@ -69,10 +78,10 @@ if __name__ == "__main__":
     dataY = []
 
     # TODO: instead of raw body, sanitize the data
-    for j, fic in enumerate(fics): 
+    for j, fic in enumerate(fics):
         print(f"Building samples {j}, {len(fic.body)}")
         n_chars += len(fic.body)
-        for i in range(0, len(fic.body) - seq_length, 1): 
+        for i in range(0, len(fic.body) - seq_length, 1):
             seq_in = fic.body[i: i + seq_length]
             seq_out = fic.body[i + seq_length]
             dataX.append([char_to_int[char] for char in seq_in])
@@ -83,8 +92,8 @@ if __name__ == "__main__":
 
     y = to_one_hot(dataY, n_vocab)
 
-    dataX = np.array(dataX) / n_vocab
-    dataX = torch.as_tensor(dataX, dtype=torch.float)
+    dataX = np.array(dataX)
+    dataX = torch.as_tensor(dataX, dtype=torch.int64)
 
     print(dataX.shape)
 
@@ -94,7 +103,7 @@ if __name__ == "__main__":
 
     nb_classes = n_vocab
 
-    ### Neural Network
+    # Neural Network
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Device being used is {device}')
@@ -102,17 +111,17 @@ if __name__ == "__main__":
     # Store the loss for each epoch
     total_loss = []
 
-    model = LSTMNet(input_size, hidden_size, num_layers, nb_classes, device, dropout).to(device)
+    model = LSTMNet(input_size, hidden_size, num_layers,
+                    nb_classes, device, dropout).to(device)
     # model = BiLSTMNet(input_size, hidden_size, num_layers, num_classes).to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.CrossEntropyLoss()
 
     train_loader = torch.utils.data.DataLoader(
-                    dataset=dataset,
-                    batch_size=batch_size,
-                    shuffle=True)
-
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=True)
 
     # training
     total_step = len(train_loader)
@@ -121,14 +130,17 @@ if __name__ == "__main__":
     for epoch in range(num_epochs):
         epoch_loss = 0
         for i, (seq, lab) in enumerate(train_loader):
-            
+
             # seq = seq.to(device)
-            seq = seq.reshape(-1, seq_length, input_size).to(device=device)
+            seq = seq.reshape(-1, seq_length, 1).to(device=device)
+            # print(f'seq: {seq.shape}')
 
             # torch.cat(seq).view(len(seq), 1, -1).to(device)
             lab = lab.to(device)
 
             outputs = model(seq)
+            # print(f'outputs: {outputs.shape}')
+            # print(f'labels: {torch.max(lab, 1)[1].shape}')
             loss = loss_fn(outputs, torch.max(lab, 1)[1])
 
             optimizer.zero_grad()
@@ -137,8 +149,8 @@ if __name__ == "__main__":
             epoch_loss += loss.item()
             if (i+1) % 100 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f} ({:.2f} s)'
-                .format(epoch+1, num_epochs, i+1, total_step,
-                loss.item(), time.time()-start))
+                      .format(epoch+1, num_epochs, i+1, total_step,
+                              loss.item(), time.time()-start))
         total_loss.append(epoch_loss / total_step)
 
     print(f"Loss for each epoch: {total_loss}")
--- predict.py
+++ predict.py
@@ -8,34 +8,37 @@ import pickle
 import sys
 
 # Hyper params
-seq_length = 100
+seq_length = 200
 batch_size = 100
-input_size = 1
+input_size = 30
 hidden_size = 100
 num_layers = 2
-num_epochs = 20
+num_epochs = 1
 learning_rate = 0.01
 dropout = 0
-nb_classes = 76
+nb_classes = 74
 
 
-model = LSTMNet(input_size, hidden_size, num_layers, nb_classes, 'cpu', dropout)
+model = LSTMNet(input_size, hidden_size, num_layers,
+                nb_classes, 'cpu', dropout)
 model.load_state_dict(torch.load("./model-state.torch"))
 model.eval()
 
+
 def to_one_hot(i, total_classes):
     return np.eye(total_classes)[i]
+
 
 fics = []
 chars = set()
 char_to_int = {}
 
 # Load Fanfics, 49999 in total
-with open("./fics.pkl", 'rb') as file: 
+with open("./fics.pkl", 'rb') as file:
     fics = pickle.load(file)
-    fics = fics[:1] # begin with only this much
+    fics = fics[:1]  # begin with only this much
 
-# Prepare Vocabulary 
+# Prepare Vocabulary
 for fic in fics:
     chars.update(set(fic.body))
 
@@ -52,10 +55,10 @@ dataX = []
 dataY = []
 
 # TODO: instead of raw body, sanitize the data
-for j, fic in enumerate(fics): 
+for j, fic in enumerate(fics):
     print(f"Building samples {j}, {len(fic.body)}")
     n_chars += len(fic.body)
-    for i in range(0, len(fic.body) - seq_length, 1): 
+    for i in range(0, len(fic.body) - seq_length, 1):
         seq_in = fic.body[i: i + seq_length]
         seq_out = fic.body[i + seq_length]
         dataX.append([char_to_int[char] for char in seq_in])
@@ -77,9 +80,8 @@ print(''.join([int_to_char[value] for value in pattern]))
 with torch.no_grad():
     # generate characters
     for i in range(1000):
-        x = np.reshape(pattern, (-1, seq_length, input_size))
-        x = x / float(n_vocab)
-        x = torch.as_tensor(x, dtype=torch.float)
+        x = np.reshape(pattern, (-1, seq_length, 1))
+        x = torch.as_tensor(x, dtype=torch.int64)
         out = model(x).view(nb_classes)
         # index = np.argmax(out).item() # read value of 1d tensor
         # print(out.shape)
@@ -87,7 +89,7 @@ with torch.no_grad():
         # top_indexes = torch.topk(out, 5, largest=True)
         probs = torch.nn.functional.softmax(out, 0)
         # index = np.random.choice(top_indexes[1])
-        index = np.random.choice(np.arange(0,nb_classes), p=probs.numpy())
+        index = np.random.choice(np.arange(0, nb_classes), p=probs.numpy())
         result = int_to_char[index]
         seq_in = [int_to_char[value] for value in pattern]
         sys.stdout.write(result)
